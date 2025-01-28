c6e2300618298833839d41afb10b2e03c7b035ed
ShadWeb <shyifou@gmail.com>
2025-01-28 21:51:14 +0330

    up-6

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Container from './Container'
-import Hamicon from '../assets/svg/burger-menu-svgrepo-com.svg'
+
 function Navbar() {
     const pathname=usePathname()
     const navlinks= [
@@ -30,8 +30,10 @@ function Navbar() {
             )))
         }
       </div>
-      <div className='w-10 h-10'>
-        <img className='w-[40px] h-[40px]' src={Hamicon} alt="" /> 
+      <div className='w-10 h-10 md:hidden'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
       </div>
       <div>
         <Link className='font-bold text-gray-800 text-2xl hover:text-cyan-500' href="./">شهرام موبد</Link>
